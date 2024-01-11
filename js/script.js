ecb3d2d43fc1e48917e2c591dd0d1f36b72aacca
Karangill2470 <karangill2470@gmail.com>
2024-01-11 11:39:06 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('mousemove', function (event) {
+    const rotatingImage = document.getElementById('rotatingImage');
+    
+    // Calculate the rotation based on the mouse position
+    const rotationValue = `rotate(${event.clientX / window.innerWidth * 360}deg)`;
+    
+    // Apply the rotation to the image
+    rotatingImage.style.transform = rotationValue;
+  });
+  
