9b59b66e47c6dafab8613ca5d7c4f559fb690908
Karangill2470 <karangill2470@gmail.com>
2024-01-12 12:55:37 -0500

    SafePortofolio

--- js/script.js
+++ js/script.js
@@ -7,3 +7,30 @@ document.addEventListener('mousemove', function (event) {
     // Apply the rotation to the image
     rotatingImage.style.transform = rotationValue;
   });
+
+
+
+
+
+
+
+
+
+
+
+  const cards = document.querySelectorAll(".card");
+
+ cards.forEach((card,index)=>{
+  card.addEventListener("click", ()=> {
+    const state  = Flip.getState(cards);
+
+    const isCardActive = card.classList.contains("active");
+    cards.forEach((otherCard, otherIdx)=>{
+      otherCard.classList.remove("active");
+      otherCard.classList.remove("is-inactive");
+      if(!isCardActive && index !== otherIdx) {
+        otherCard.classList.add("is-inactive");
+      }
+    })
+  })
+ })
