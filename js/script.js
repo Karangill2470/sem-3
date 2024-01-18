f5e467a1ffc0fb7bf88bf496df6647f2fe50269c
Karangill2470 <karangill2470@gmail.com>
2024-01-17 21:43:06 -0500

    links

--- js/script.js
+++ js/script.js
@@ -1,71 +1,53 @@
-document.addEventListener('mousemove', function (event) {
-    const rotatingImage = document.getElementById('rotatingImage');
-    
-    // Calculate the rotation based on the mouse position
-    const rotationValue = `rotate(${event.clientX / window.innerWidth * 360}deg)`;
-    
-    // Apply the rotation to the image
-    rotatingImage.style.transform = rotationValue;
-  });
 
 
 
+const cards = document.querySelectorAll(".card");
 
-
-
-
-
-
-
-
-  const cards = document.querySelectorAll(".card");
-
- cards.forEach((card,index)=>{
-  card.addEventListener("click", ()=> {
-    const state  = Flip.getState(cards);
-
+cards.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    const state = Flip.getState(cards);
     const isCardActive = card.classList.contains("active");
-    cards.forEach((otherCard, otherIdx)=>{
+
+    cards.forEach((otherCard, otherIdx) => {
       otherCard.classList.remove("active");
       otherCard.classList.remove("is-inactive");
-      if(!isCardActive && index !== otherIdx) {
+
+      if (!isCardActive && index !== otherIdx) {
+        gsap.to(otherCard, { duration: 0.5, width: "auto", height: "auto" });
         otherCard.classList.add("is-inactive");
       }
-    })
-  })
- })
+    });
+
+    gsap.to(card, { duration: 0.5, width: "100%", height: "100%" });
+    card.classList.toggle("active");
+  });
+});
 
 
 
 
 //  nav links 
-
 gsap.registerPlugin(Flip);
 
-const links = document.querySelectorAll("nav ul li a");
+const links = document.querySelectorAll(".nav-item a");
 const activeNav = document.querySelector(".active-nav");
 
 links.forEach((link) => {
     link.addEventListener("click", (e) => {
-        e.preventDefault();
-
         // Reset color for all links
         gsap.to(links, { color: "#252525" });
-
+        
         // Set color for the clicked link
         gsap.to(link, { color: "#385ae0" });
 
         // Move the activeNav under the clicked link
         const state = Flip.getState(activeNav);
-        activeNav.style.width = `${link.clientWidth}px`; // Adjust width if necessary
+        link.appendChild(activeNav);
         Flip.from(state, {
-            duration: 0.5,
-            absolute: true,
-            ease: "power2.out",
-            onComplete: () => {
-                link.appendChild(activeNav);
-            },
-        });
+            duration:1.5,
+            absolute:true,
+            ease: "elastic.out(1,0.5)"
+        })
     });
 });
 
